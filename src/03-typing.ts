30cd37d6b1d6e2ba0fa3b1f30e2440c048c6a780
htmtmx <cesar.pineda@daksha.mx>
2023-12-27 11:44:12 -0600

    07 - ✨ Add typing and demo files

---
+++ src/03-typing.ts
@@ -0,0 +1,13 @@
+(() => {
+  let productName = 'Monitor 24"';
+  let productPrice = 23;
+
+  productName = 'Monitor 32"';
+
+  productName.toLowerCase();
+
+  const productStock = 10000;
+
+  const productName2 = 'Monitor 24"';
+
+})();
---
+++ src/demo-03.ts
@@ -0,0 +1,11 @@
+//@ts-check
+let productName = 'Monitor 24"';
+let productPrice = 23;
+
+productName = 'Monitor 32"';
+
+productName.toLowerCase();
+
+const productStock = 10000;
+
+const productName2 = 'Monitor 24"';
