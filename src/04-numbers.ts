6c4761d41f4bef2de34b6b88ae20bd695b08ee95
htmtmx <cesar.pineda@daksha.mx>
2023-12-27 13:10:00 -0600

    08- ✨ Number types in Typescript

---
+++ src/04-numbers.ts
@@ -0,0 +1,24 @@
+(() => {
+  let productPrice = 100;
+  productPrice = 12.05;
+  console.log('productPrice', productPrice);
+
+  let customerAge : number = 25;
+  // customerAge = customerAge + '1'; == 251
+  customerAge = customerAge + 1; // == 26
+
+  let productsInStock: number;
+//   productsInStock = 3;
+  console.log("productsInStock", productsInStock);
+  if (productsInStock > 10) {
+    console.log("We have enough products");
+  }
+  let discount = parseInt("123");
+    console.log("discount", discount);
+
+    let hex: number = 0xf00d;
+    console.log("hex", hex);
+
+    let binary: number = 0b1010;
+    console.log("binary", binary);
+})();
